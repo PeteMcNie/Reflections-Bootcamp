2eb8cbd36e87fd899ea154c1a2aa062c6d3f5967
petemcnie <petemcnie0@gmail.com>
2020-06-13 13:48:19 +1200

    Practice attentive listening answered

---
+++ index.js
@@ -0,0 +1,22 @@
+//
+
+
+
+//##### Practice attentive listening and intentional clarifying questions: #####
+
+// I have show that I have practiced attentive listening during class when we were 
+// discussing setting up server side test routes and I enquired to why we were using 
+// these options to view errors:
+
+// console.log(err) - a way of handling an error, what we are going to do with it
+
+// console.error(err) - a way of handling an error, what we are going to do with it
+
+// if (err) return callback(err) - async operations, use the err first pattern of completing operations
+
+// throw new Error  - stops program and finds a catch to execute, delivers the error synchronously
+
+// - catch(err) - required if an err is ‘thrown’ otherwise the process will exit and server will restart (crash).
+
+// This was clarified for me when we had a discussion on errors and the situations such 
+// as functions, async functions and Promises which all affect what syntax we use in error situations.
